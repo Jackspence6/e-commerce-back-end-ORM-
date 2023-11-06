96b388d27b226ea36046d6a83ee01bdc34ef5845
Jack Spence <jackspence123456@gmail.com>
2023-11-06 17:12:08 +0200

    Added route to delete single product by its Id.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -125,8 +125,23 @@ router.put("/:id", async (req, res) => {
 		});
 });
 
-router.delete("/:id", (req, res) => {
-	// delete one product by its `id` value
+// delete one product by its `id` value
+router.delete("/:id", async (req, res) => {
+	try {
+		const productData = await Product.destroy({
+			where: {
+				id: req.params.id,
+			},
+		});
+		// Check to see if there is corresponding product to the Id being deleted
+		if (!productData) {
+			res.status(404).json("No product found matching this Id!");
+			return;
+		}
+		res.status(200).json(productData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
